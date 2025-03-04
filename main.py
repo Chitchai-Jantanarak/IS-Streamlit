021797652e95b1cafff4331e8119704b2d48f54b
Chitchai-Jantanarak <s6604062663051@email.kmutnb.ac.th>
2025-03-04 21:38:58 +0700

    Commit 3-4-2

---
+++ main.py
@@ -0,0 +1,9 @@
+import streamlit as st
+# from src.pages import overview, visualization, prediction
+
+def main():
+    st.sidebar.title("TEST")
+
+
+if __name__ == "__main__":
+    main()
--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="streamlit-project",
+    name="streamlit-business-dashboard",
     version="0.1.0",
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
     install_requires=[
-        'streamlit',
-        'pandas',
-        'plotly',
+        'streamlit==1.32.0',
+        'pandas==2.2.1',
+        'plotly==5.18.0',
+        'numpy==1.26.4',
+        'matplotlib==3.8.3',
+        'scikit-learn==1.4.1',
     ],
 )

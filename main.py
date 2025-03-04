8867adcfd8fd51a7a945afb19e290b408f813164
Chitchai-Jantanarak <s6604062663051@email.kmutnb.ac.th>
2025-03-04 22:51:09 +0700

    Commit 3-4-4

--- main.py
+++ main.py
@@ -1,8 +1,32 @@
 import streamlit as st
+import pathlib
 # from src.pages import overview, visualization, prediction
 
 def main():
-    st.sidebar.title("TEST")
+    st.set_page_config(page_title="Intelligent System Project", page_icon=":robot_face:")
+
+    sidebar()
+
+    st.title("Intelligent System Project")
+    st.write("Navigate through the pages to explore different ML models and explanations.")
+
+def sidebar() -> None:
+    st.sidebar.title("Pages")
+
+    # Section 1
+    st.sidebar.subheader("1. Machine Learning")
+    st.sidebar.link_button(label="Explaination", url="https://openai.com", type="tertiary")
+    st.sidebar.link_button(label="Model", url="#credit-card", type="tertiary")
+    
+    st.sidebar
+    # Section 2
+    st.sidebar.subheader("2. Neural Networks")
+    st.sidebar.link_button("Explaination", "https://openai.com", type="tertiary")
+    st.sidebar.link_button("Model", "https://openai.com", type="tertiary")
+    st.sidebar.markdown("[Credit Card](#credit-card)")
+    st.sidebar.markdown("[PayPal](#paypal)")
+
+    st.sidebar.markdown(" ---------------------- ")
 
 
 if __name__ == "__main__":
